69b66d83a22ad1471c5bd5a8cb76d539ad202ac4
Colin Walker <colin@sendwithus.com>
2019-10-03 13:41:55 -0700

    Install all libs

--- dist.sh
+++ dist.sh
@@ -2,27 +2,46 @@
 
 set -e
 
+VENDOR="/workspace/vendor"
+
 LIBXSLT="libxslt-1.1.28"
 LIBXML2="libxml2-2.9.1"
-LIB="/workspace/dist"
 
-mkdir /workspace/dist
+LXML="lxml-3.1beta1.tar.gz"
+PREMAILER="premailer-25a9687d2808d3498a63644a3915daddf2474994.zip"
+CSSSELECT="cssselect-0.7.1.tar.gz"
+
+LIB="/workspace/dist"
+LIBPY="$LIB/py"
+
+mkdir -p /workspace/dist/py
 cd /tmp
 
-tar -xzf /workspace/$LIBXML2.tar.gz
+tar -xzf $VENDOR/$LIBXML2.tar.gz
 pushd $LIBXML2
 ./configure --prefix=$LIB
 make
 make install
 popd
 
-tar -xzf /workspace/$LIBXSLT.tar.gz
+tar -xzf $VENDOR/$LIBXSLT.tar.gz
 pushd $LIBXSLT
 ./configure --prefix=$LIB
 make
 make install
 popd
 
+# Make sure LXML is built against headers from our fixed package:
+export LD_LIBRARY_PATH="$LIB/lib"
+
+pip install --no-deps $VENDOR/$LXML --target $LIBPY
+pip install --no-deps $VENDOR/$PREMAILER --target $LIBPY
+pip install --no-deps $VENDOR/$CSSSELECT --target $LIBPY
+
 pushd $LIB
 tar -zcf /workspace/dist.tar.gz .
 popd
+
+rm -r $LIB
+
+echo "-------- DONE --------"
