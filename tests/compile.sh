9021aa6e350b5b501a990c77faab23426811ad62
Colin Walker <colin@sendwithus.com>
2019-10-03 13:54:22 -0700

    Fix url tests

--- tests/compile.sh
+++ tests/compile.sh
@@ -2,12 +2,13 @@
 
 set -e
 
-/workspace/bin/compile /tmp /tmp /tmp
+mkdir /app
+/workspace/bin/compile /app /app /app
 
-export PYTHONPATH=$(cat /tmp/PYTHONPATH)
-export LD_LIBRARY_PATH=$(cat /tmp/LD_LIBRARY_PATH)
+export PYTHONPATH=$(cat /app/PYTHONPATH)
+export LD_LIBRARY_PATH=$(cat /app/LD_LIBRARY_PATH)
 
-cd /tmp
+cd /app
 
 python -c 'from lxml import etree'
 python -c 'html = "<html><head></head><body>Dylan <> Colin 1:1</body></html>"; from premailer import Premailer; print Premailer(html, keep_style_tags=True, remove_classes=False, strip_important=False, load_external=True).transform()' > actual.html
